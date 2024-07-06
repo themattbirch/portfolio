8f8dd853a812db52af1128728d213bcbc282cb08
Matt B <matt@optimizewebsolutions.com>
2024-07-06 00:26:49 -0400

    All all new untracked files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  content: {
+    // ... other content settings
+    fields: [
+      { name: "slug", type: "string" }, // Add this line
+    ],
+  },
 });
