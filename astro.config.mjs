d8a8d31fa2ba850e8d3defec98ff4ed848eebbe1
Matt B <matt@optimizewebsolutions.com>
2024-08-01 01:11:10 -0400

    Layout component name changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte"
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   site: "https://mattbirch.co",
+  image: {
+    service: { entrypoint: "astro/assets/services/sharp" },
+  },
   content: {
     // ... other content settings
-    fields: [
-      { name: "slug", type: "string" }, // Add this line
-    ],
+    fields: [{ name: "slug", type: "string" }],
   },
 });
