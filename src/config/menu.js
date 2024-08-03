916ce7913c92d27359b2a2a9b9806170eefbe28f
Matt B <matt@optimizewebsolutions.com>
2024-08-03 00:26:56 -0400

    Nav Bar finished

---
+++ src/config/menu.js
@@ -0,0 +1,13 @@
+export const menuItems = [
+  { title: "Home", path: "/" },
+  { title: "About", path: "/about" },
+  {
+    title: "Services",
+    children: [
+      { title: "Web Design", path: "/services/web-design" },
+      { title: "SEO", path: "/services/seo" },
+      { title: "Branding", path: "/services/branding" },
+    ],
+  },
+  { title: "Contact", path: "/contact" },
+];
