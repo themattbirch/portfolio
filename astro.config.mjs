2f72c394cce3286bc3eaee7de890f3b2ca0b5966
Matt B <matt@optimizewebsolutions.com>
2024-08-15 18:14:07 -0400

    Testimonials 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   site: "https://mattbirch.co",
@@ -11,7 +10,6 @@ export default defineConfig({
     service: { entrypoint: "astro/assets/services/sharp" },
   },
   content: {
-    // ... other content settings
     fields: [{ name: "slug", type: "string" }],
   },
   build: {
