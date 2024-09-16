3e537c14d9f66fb61618ece34a15026120b24734
Matt B <matt@optimizewebsolutions.com>
2024-09-16 19:25:03 -0400

    Newish

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [tailwind(), svelte(), react(), icon()],
   site: "https://mattbirch.co",
-  image: {
-    service: { entrypoint: "astro/assets/services/sharp" },
-  },
+  
   content: {
     fields: [{ name: "slug", type: "string" }],
   },
